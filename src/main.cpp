fb4270a5b57df517dd565330eab8c7d332655ddb
José Raimundo <j.zmais@gmail.com>
2018-03-29 11:30:11 -0300

    Organização do input

--- src/main.cpp
+++ src/main.cpp
@@ -11,39 +11,80 @@
 #include <cmath>
 #include <stdio.h>
 #include <ctime>
+#include <stdlib.h>
+#include <cstring>
+#include <sstream> 
 #include <fstream>
+#include <vector> 
 
 using namespace std;
-#define row 2160
-#define col 3840
+
+void usage(){
+	cout << "Use the command (Exemple): \n"
+			"./bin/main -ref [reference video] -eva [evaluated video] -wht [frames width] -htg [frames height] -f [number of frames]\n" 
+			"Example:\n"
+			"./bin/main video_reference.yuv video_test.yuv -wht 3840 -htg 2160 -f 120"<< endl;
+}
 
 
+int converterStrToInt(string str){
+    stringstream convert(str); 
+    int temp_nuber;
+    if(!(convert >> temp_nuber))
+        return 0;
+    return temp_nuber;
+}
 
 
 int main (int argc, char const* argv[]){
+	if (argc < 4){
+		usage();
+		return 0;
+	}
+
+	//create variables and set the defalt parameters values
+	int numFrames 	= 130,
+		row 		= 2160,	
+		col 		= 3840; 
+
+	string ref_path = "", eva_path = "";
 
 	Video reference,test;
-
 	PSNR  psnr;
 	SSIM ssim;
 	PWSSIM pwssim;
 
 	// set input pareameters
-	
-	
+	vector<string> command_input;
+    for (int i = 0; i < argc; i++){
+        command_input.push_back(argv[i]);
+    }
+
+
+// set input pareameters
+	for (int i = 0; i < argc; i++){
+		if (command_input[i].compare("-f") == 0){
+			numFrames = converterStrToInt(command_input[i+1]);
+		}else if (command_input[i].compare("-wht") == 0){
+			row = converterStrToInt(command_input[i+1]);
+		}else if (command_input[i].compare("-htg") == 0){
+			col = converterStrToInt(command_input[i+1]);
+		}else if (command_input[i].compare("-eva") == 0){
+			eva_path = command_input[i+1];
+		}else if (command_input[i].compare("-ref") == 0){
+			ref_path =  command_input[i+1];
+		}
+	}
+
+
 	reference.setCoefs(2,2,row,col);
-	reference.openFile(argv[1],row,col);
+	reference.openFile(ref_path.c_str(),row,col);
 	test.setCoefs(2,2,row,col);
 	string entrada = "";
-	
-	test.openFile(argv[2],row,col);
-	
-
+	test.openFile(eva_path.c_str(),row,col);
 
 	
-	int numFrames = test.returnFrames();
 	cout << "Colunas: " << col << " Linhas: " << row << " Frames: " << numFrames << endl;
-
 
 	/**/
 	cout<< "Entrou na PSNR\n";
